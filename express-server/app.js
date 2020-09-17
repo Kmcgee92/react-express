053d5fefe7e649c4d8e8cef2c6edb4bbc798b30b
kmcgee92 <kaseycmcgee@gmail.com>
2020-09-17 16:38:49 -0600

    mvp react/express

---
+++ express-server/app.js
@@ -0,0 +1,20 @@
+// ./app.js
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/tweets', (req, res) => {
+    res.json('tweets')
+})
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = 9000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ express-server/public/precache-manifest.bb494859846fbd79f17fd3d83010088e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2227299f75a7a8007007aac5736f88b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d6fc3205cd0d637e758",
+    "url": "/static/css/main.5c5248ef.chunk.css"
+  },
+  {
+    "revision": "77d5b9467b19a4866363",
+    "url": "/static/js/2.de291cfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de291cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6fc3205cd0d637e758",
+    "url": "/static/js/main.645aee3e.chunk.js"
+  },
+  {
+    "revision": "2478b59d780a96d7dc62",
+    "url": "/static/js/runtime-main.dbd98963.js"
+  },
+  {
+    "revision": "45f7f4d2e733638d130e2db7dc7b6721",
+    "url": "/static/media/react-builds-cat.45f7f4d2.png"
+  }
+]);
